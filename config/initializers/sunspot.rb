6b06b045ef07fe852bfb478cfbe89353c168795f
Sergei Tsoganov <sergeit6@gmail.com>
2015-06-16 09:58:33 +0300

    '16062015001'

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,6 +1,6 @@
 #init
 #uncomment this before deploy!!!
 
-#Sunspot.config.solr.url = 'http://maricavor:phoenix32@solr-webauto.rhcloud.com/solr'
+Sunspot.config.solr.url = 'http://maricavor:phoenix32@solr-webauto.rhcloud.com/solr'
 
 ###############
