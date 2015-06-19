a7813e067b663ba84ba18240c178704d0f6be3a9
Sergei Tsoganov <sergeit6@gmail.com>
2015-06-19 19:29:10 +0300

    ddd

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -68,7 +68,7 @@ class ImageUploader < CarrierWave::Uploader::Base
       model.class.to_s.underscore=="picture"
     end
   end
-
+  ###
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
