87189353306ed07230a2808695351d514895e5ad
Sergei Tsoganov <sergeit6@gmail.com>
2015-06-19 21:10:31 +0300

    carrierwave

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage = :file
-    config.enable_processing = true
-    config.root = ENV['OPENSHIFT_DATA_DIR']
-    config.cache_dir = config.root + 'uploads'
-  end    
-end
