d8509ade01945fb218556060b730e20ec7a86515
Sergei Tsoganov <sergeit6@gmail.com>
2015-06-20 08:28:29 +0300

    dkdkd

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     #for priduction--> "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.vehicle.id}/#{model.id}"
   end
 
