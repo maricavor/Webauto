4ee5a4897ee6ea08a8567657de7e37d83395a661
Sergei Tsoganov <sergeit6@gmail.com>
2015-06-20 09:31:42 +0300

    '333'

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    #for priduction--> "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.vehicle.id}/#{model.id}"
+    #for production--> "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.vehicle.id}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
