427ff5c5b9cf3089b9adab35de39aeefd0e33d60
maricavor <sergeit6@gmail.com>
2016-01-31 10:18:03 +0200

    31012016001

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -10,7 +10,7 @@ config = YAML.load config_template.result(binding)
 Resque.logger = MonoLogger.new(File.open("#{Rails.root}/log/resque.log", "w+"))
 Resque.logger.formatter = Resque::VeryVerboseFormatter.new
 #uncomment this before deploy!!!
-Resque.redis = Redis.new(config[Rails.env])
-#Resque.redis = Redis.new(:host => ENV['OPENSHIFT_REDIS_HOST'], :port => ENV['OPENSHIFT_REDIS_PORT'], :password => ENV['REDIS_PASSWORD'], :thread_safe => true)
+#Resque.redis = Redis.new(config[Rails.env])
+Resque.redis = Redis.new(:host => ENV['OPENSHIFT_REDIS_HOST'], :port => ENV['OPENSHIFT_REDIS_PORT'], :password => ENV['REDIS_PASSWORD'], :thread_safe => true)
 Resque.schedule = schedule
 Resque.redis.namespace = "<a href='http://localhost:3000' style= 'text-decoration:none;color:#cccccc;'>Webauto</a>"
