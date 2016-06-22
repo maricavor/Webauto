9b29594b7a999d108511ec30b65d88d9defd2753
maricavor <sergeit6@gmail.com>
2016-06-22 23:40:47 +0300

    configuring cron jobs1

--- .openshift/cron/minutely/date.sh
+++ .openshift/cron/minutely/date.sh
@@ -1,1 +1,3 @@
-date >> $OPENSHIFT_REPO_DIR/log/date.txt
+/bin/bash -l -c 'cd $OPENSHIFT_REPO_DIR && bundle exec script/rails runner -e production '\''Vehicle.lalala'\'' >> log/cron_log.log 2>> log/cron_error_log.log'
+
+
