2dfa4ac4ac284691ab86fdfd6e0f0d042fe75e16
maricavor <sergeit6@gmail.com>
2016-09-03 23:17:28 +0300

    03092016001

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require t-func
-//= require bootstrap
 //= require bootstrap-datepicker
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.et.js
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.en-GB.js
