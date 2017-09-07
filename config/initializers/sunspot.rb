f4dbf6d33f17cd9202ab9f8437808fe4ea2baa35
maricavor <sergeit6@gmail.com>
2017-09-07 15:22:45 +0300

    07092017003

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -4,4 +4,4 @@
 #Sunspot.config.solr.url = 'http://maricavor:phoenix32@solr-webauto.rhcloud.com/solr'
 Sunspot.config.solr.url = 'http://146.185.140.73:8983/solr'
 #end
-####################
+#####################
