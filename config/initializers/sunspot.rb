7d44207858b8ff829d4c431624f649748b51dc37
maricavor <sergeit6@gmail.com>
2017-09-07 09:38:49 +0300

    07092017001

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -2,6 +2,6 @@
 #uncomment this before deploy!!!
 if Rails.env.production?
 #Sunspot.config.solr.url = 'http://maricavor:phoenix32@solr-webauto.rhcloud.com/solr'
-Sunspot.config.solr.url = 'http://localhost:8983/solr'
+Sunspot.config.solr.url = 'http://localhost:8983/solr/vehicles'
 end
 ###############
