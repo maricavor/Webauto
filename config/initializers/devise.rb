97f21638b182614ed98cb4a2103df90404105462
maricavor <sergeit6@gmail.com>
2017-09-17 23:26:55 +0300

    17092017002

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   require "omniauth-facebook"
   require 'omniauth-google-oauth2'
   config.omniauth :google_oauth2, "900864775269-g693th1ub54hkfcuu8hs84i0thpo8dfr.apps.googleusercontent.com", "ra7rQr16FEXZCmnEqzHMqMeR", { }
-  config.omniauth :facebook, "310281229120060", "3f15e86c973d59a29b62397eac62a0d5"
+  config.omniauth :facebook, "310281229120060", "3f15e86c973d59a29b62397eac62a0d5", token_params: { parse: :json }
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
